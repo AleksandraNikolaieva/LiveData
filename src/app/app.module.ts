ba7119f7a667207e6b4d4c31f9e8eb13d8be8c74
Dmitriy Kurilenko <golah30@gmail.com>
2019-07-09 02:10:37 +0300

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { CreditsComponent } from "./credits/credits.component";
@@ -11,11 +10,8 @@ import { TimeGonePipe } from "./pipes/time-gone.pipe";
 import { CommaNumberPipe } from "./pipes/comma-number.pipe";
 import { MainPageComponent } from "./main-page/main-page.component";
 import { AppRoutingModule } from "./app-routing.module";
-<<<<<<< HEAD
 import { SharedModule } from "./shared/shared.module";
-=======
-import { CreditsOffersComponent } from './purchase-page/credits-offers/credits-offers.component';
->>>>>>> 256940c658130fce91d1218528211098b1c8f52b
+import { CreditsOffersComponent } from "./purchase-page/credits-offers/credits-offers.component";
 
 @NgModule({
     declarations: [
