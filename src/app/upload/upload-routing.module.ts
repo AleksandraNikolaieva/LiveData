331c3aa3223567b204ec5222e53b409255ca4f91
Dmitriy Kurilenko <golah30@gmail.com>
2019-07-09 20:24:25 +0300

    map fields page

--- src/app/upload/upload-routing.module.ts
+++ src/app/upload/upload-routing.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { UploadListComponent } from "./upload-list/upload-list.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UploadListComponent } from './upload-list/upload-list.component';
+import { MapFieldsComponent } from './map-fields/map-fields.component';
 
 const routes: Routes = [
     {
-        path: "",
+        path: '',
         component: UploadListComponent,
-        pathMatch: "full"
+        pathMatch: 'full'
+    },
+    {
+        path: 'clean',
+        component: MapFieldsComponent
     }
 ];
 
