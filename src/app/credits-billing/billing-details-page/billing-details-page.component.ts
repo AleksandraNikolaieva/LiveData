6514ff893cc88d130402d44696a4fa79798773f2
Edgar0055 <bebkovan@gmail.com>
2019-07-09 12:23:57 +0300

    started crafting credits-billing

--- src/app/credits-billing/billing-details-page/billing-details-page.component.ts
+++ src/app/credits-billing/billing-details-page/billing-details-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-billing-details-page',
   templateUrl: './billing-details-page.component.html',
-  styleUrls: ['./billing-details-page.component.sass']
+  styleUrls: ['./billing-details-page.component.scss']
 })
 export class BillingDetailsPageComponent implements OnInit {
 
-  constructor() { }
+  billingForm: FormGroup;
 
-  ngOnInit() {
-  }
+  constructor(private formBuilder: FormBuilder) { }
+
+ngOnInit() {
+this.billingForm = this.formBuilder.group({
+name: ['', Validators.required],
+cardNumber: ['', [Validators.required, Validators.maxLength(12), Validators.minLength(12)]],
+expMonth: ['', Validators.required, Validators.maxLength(2), Validators.minLength(2)],
+expYear: [['', Validators.required, Validators.maxLength(4), Validators.minLength(4)]],
+cvv: [['', Validators.required, Validators.maxLength(3), Validators.minLength(3)]]
+});
+}
 
 }
