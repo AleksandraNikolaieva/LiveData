7fdf3a5f2e37febd58655a782872a134fc7d133c
Edgar0055 <bebkovan@gmail.com>
2019-07-09 21:15:13 +0300

    formatting

--- src/app/credits-billing/credits-offers/credits-offers.component.ts
+++ src/app/credits-billing/credits-offers/credits-offers.component.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { SelectedPackageService } from '../services/selected-package.service';
+import { Component, OnInit, Input, Output } from "@angular/core";
+import { SelectedPackageService } from "../services/selected-package.service";
 
 @Component({
-  selector: 'app-credits-offers',
-  templateUrl: './credits-offers.component.html',
-  styleUrls: ['./credits-offers.component.scss']
+    selector: "app-credits-offers",
+    templateUrl: "./credits-offers.component.html",
+    styleUrls: ["./credits-offers.component.scss"]
 })
 export class CreditsOffersComponent implements OnInit {
+    constructor(private selectedPackage: SelectedPackageService) {}
+    optionSelected: number;
+    @Input() packages: Array<Package>;
 
-  constructor(private selectedPackage: SelectedPackageService) { }
-   optionSelected: number;
-  @Input() packages: Array<Package>;
+    ngOnInit() {}
 
-
-  ngOnInit() {
-  }
-
-  modelChanged() {
-    this.selectedPackage.package = this.packages.find(el => el.value == this.optionSelected);
-  }
+    modelChanged() {
+        this.selectedPackage.package = this.packages.find(
+            el => el.value == this.optionSelected
+        );
+    }
 }
