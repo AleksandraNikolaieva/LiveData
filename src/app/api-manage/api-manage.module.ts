8e982ca9eece9b7f2c90964fe22256cdae170f28
Aleksandra Nikolaieva <oleksandranikolaieva@gmail.com>
2019-07-12 11:51:55 +0300

    implemented token page

--- src/app/api-manage/api-manage.module.ts
+++ src/app/api-manage/api-manage.module.ts
@@ -4,9 +4,18 @@ import { ApiManageComponent } from './api-manage/api-manage.component';
 import { ApiSetupComponent } from './api-setup/api-setup.component';
 import { ApiManageRoutingModule } from './api-manage-routing.module';
 import { ApiUsageComponent } from './api-usage/api-usage.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-    declarations: [ApiManageComponent, ApiSetupComponent, ApiUsageComponent],
-    imports: [CommonModule, ApiManageRoutingModule]
+    declarations: [
+        ApiManageComponent,
+        ApiSetupComponent,
+        ApiUsageComponent
+    ],
+    imports: [
+        CommonModule,
+        ApiManageRoutingModule,
+        SharedModule
+    ]
 })
 export class ApiManageModule {}
