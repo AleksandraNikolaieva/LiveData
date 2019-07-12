23b71fa0689bd8cdb0757f87c0209edefaab3ada
Edgar0055 <bebkovan@gmail.com>
2019-07-12 19:54:28 +0300

    returned 2nd popup to the main page

--- src/app/shared/report-popup/report-popup.component.ts
+++ src/app/shared/report-popup/report-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { Report } from 'src/app/models';
 import { report } from '../../mockData';
+import { AuthService } from 'src/app/auth/service/auth.service';
 
 @Component({
     selector: 'app-report-popup',
@@ -8,15 +9,31 @@ import { report } from '../../mockData';
     styleUrls: ['./report-popup.component.scss']
 })
 export class ReportPopupComponent implements OnInit {
+    downloadPopup = false;
+    inputActive = false;
+    userEmail: string;
     @Output() closePopup: EventEmitter<any> = new EventEmitter();
+    @ViewChild('emailInput') emailField: ElementRef;
     @Input() report: Report = report;
 
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
     ngOnInit() {
+        this.userEmail = this.authService.email;
     }
 
     closedPopup() {
         this.closePopup.emit();
+    }
+
+    showDownload() {
+        this.downloadPopup = true;
+    }
+
+    editEmail() {
+        this.inputActive = true;
+        setTimeout(() => {
+            this.emailField.nativeElement.focus();
+          }, 0);
     }
 }
