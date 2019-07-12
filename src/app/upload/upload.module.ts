15253312b311c3dd991030aeff6a302668bd5668
Dmitriy Kurilenko <golah30@gmail.com>
2019-07-12 13:22:48 +0300

    upload details and list style fixes

--- src/app/upload/upload.module.ts
+++ src/app/upload/upload.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UploadListComponent } from './upload-list/upload-list.component';
 import { UploadFileComponent } from './upload-file/upload-file.component';
-import { UploadHeaderComponent } from './upload-header/upload-header.component';
 import { UploadDetailsComponent } from './upload-details/upload-details.component';
 import { UploadFilePreviewComponent } from './upload-file-preview/upload-file-preview.component';
 import { UploadRoutingModule } from './upload-routing.module';
@@ -14,16 +13,10 @@ import { FormsModule } from '@angular/forms';
     declarations: [
         UploadListComponent,
         UploadFileComponent,
-        UploadHeaderComponent,
         UploadDetailsComponent,
         UploadFilePreviewComponent,
         MapFieldsComponent
     ],
-    imports: [
-        CommonModule,
-        UploadRoutingModule,
-        SharedModule,
-        FormsModule
-    ]
+    imports: [CommonModule, UploadRoutingModule, SharedModule, FormsModule]
 })
 export class UploadModule {}
