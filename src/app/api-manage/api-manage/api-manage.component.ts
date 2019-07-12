d367844e0a08a1fa64be41cea8b63e7de9ceffaf
Aleksandra Nikolaieva <oleksandranikolaieva@gmail.com>
2019-07-12 15:33:37 +0300

    add form

--- src/app/api-manage/api-manage/api-manage.component.ts
+++ src/app/api-manage/api-manage/api-manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-api-manage',
@@ -7,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApiManageComponent implements OnInit {
     title = 'Manage API';
+    apiControl: FormControl;
 
     constructor() { }
 
     ngOnInit() {
+        this.apiControl = new FormControl(null);
     }
-
 }
